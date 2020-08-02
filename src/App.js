7c22bd3a4ff6161bef359d4453888859c05ae73a
Darbaz Ali <darbaz.a.h@gmail.com>
2020-08-02 11:20:03 +0300

    define timer functions

--- src/App.js
+++ src/App.js
@@ -3,24 +3,22 @@ import { render } from "react-dom";
 
 import BreakController from "./components/BreakController";
 import SessionController from "./components/SessionController";
-import Timer from './components/Timer';
-import Audio from './components/Audio';
+import Timer from "./components/Timer";
+import Audio from "./components/Audio";
 
 const App = () => {
-
   /*====================================
     DEFINE HOOKS
   =====================================*/
   const [_break, setBreak] = useState(5);
   const [_timer, setTimer] = useState(25);
 
-  const [breakTime, setBreakTime] = useState(_break * 60 );
-  const [session, setSession] = useState(_timer * 60 );
+  const [breakTime, setBreakTime] = useState(_break * 60);
+  const [session, setSession] = useState(_timer * 60);
 
   const [isRunning, setIsRunning] = useState(false);
-  const [status, setStatus] = useState('session');
+  const [status, setStatus] = useState("session");
   // end of hooks
-
 
   /*====================================
     DEFINE VARIABLES
@@ -28,40 +26,90 @@ const App = () => {
   const audio = useRef();
   let interval = null;
 
-  
+  /*====================================
+    TIMER FUNCTIONS
+  =====================================*/
 
+  const clockify = (duration) => {
+    let timer = duration,
+      minutes,
+      seconds;
+
+    minutes = parseInt(timer / 60, 10);
+    seconds = parseInt(timer % 60, 10);
+
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    let time = minutes + ":" + seconds;
+
+    if (--timer < 0) {
+      timer = 0;
+    }
+
+    return time;
+  };
+
+  const tick = () => {
+    if (isRunning === true) {
+      if (status === "session") {
+        setTimer((timer) => timer - 1);
+      } else if (status === "break") {
+        setBreak((_break) => _break - 1);
+      }
+    }
+  };
+
+  const start = () => {
+    setIsRunning(true);
+    tick()
+  }
+
+  const pause = () => {
+    setIsRunning( false )
+    clearInterval( interval );
+  }
+
+  const reset = () => {
+    setIsRunning( false );
+    clearInterval( interval )
+    setStatus('session')
+    setTimer( 1500 )
+    setBreak(300);
+    setSession(25)
+    setBreakTime(5)
+    audio.current.pause();
+  }
 
   /*====================================
     DEFINE HANDLERS
   =====================================*/
   const sessionINC = () => {
-    if ( session < 60 ) {
-      setSession( session + 1)
-      setTimer( _timer + 60 )
+    if (session < 60) {
+      setSession(session + 1);
+      setTimer(_timer + 60);
     }
-  }
+  };
 
   const sessinDEC = () => {
-    if ( session > 1 ) {
-      setSession( session - 1 )
-      setTimer( _timer - 60 )
+    if (session > 1) {
+      setSession(session - 1);
+      setTimer(_timer - 60);
     }
-  }
+  };
 
   const breakINC = () => {
-    if ( breakTime < 60 ) {
-      setBreakTime( breakTime + 1 )
-      setBreak( _break + 60 );
+    if (breakTime < 60) {
+      setBreakTime(breakTime + 1);
+      setBreak(_break + 60);
     }
-  }
+  };
 
   const breakDEC = () => {
-    if ( breakTime > 1 ) {
-      setBreakTime( breakTime - 1 )
-      setBreak( _break - 60 );
+    if (breakTime > 1) {
+      setBreakTime(breakTime - 1);
+      setBreak(_break - 60);
     }
-  }
-
+  };
 
   return (
     <div>
@@ -69,9 +117,7 @@ const App = () => {
       <Timer />
       <BreakController />
       <SessionController />
-      <Audio 
-        audio={audio}
-      />
+      <Audio audio={audio} />
     </div>
   );
 };
--- src/components/Audio.js
+++ src/components/Audio.js
@@ -1,15 +1,15 @@
 /* eslint-disable jsx-a11y/media-has-caption */
-import React from 'react'
+import React from "react";
 
 const Audio = ({ audio }) => {
-    return (
-        <audio
-        id="beep"
-        preload="auto"
-        src="https://raw.githubusercontent.com/freeCodeCamp/cdn/master/build/testable-projects-fcc/audio/BeepSound.wav"
-        ref={ audio }
-      />
-    )
-}
+  return (
+    <audio
+      id="beep"
+      preload="auto"
+      src="https://raw.githubusercontent.com/freeCodeCamp/cdn/master/build/testable-projects-fcc/audio/BeepSound.wav"
+      ref={audio}
+    />
+  );
+};
 
 export default Audio;
