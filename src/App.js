209bfa4348b1d6dc4c7afad9815f843e2821b5cb
Darbaz Ali <darbaz.a.h@gmail.com>
2020-08-02 10:51:30 +0300

    define handler functions

--- src/App.js
+++ src/App.js
@@ -11,8 +11,8 @@ const App = () => {
   /*====================================
     DEFINE HOOKS
   =====================================*/
-  const [_break, set_break] = useState(5);
-  const [_timer, set_timer] = useState(25);
+  const [_break, setBreak] = useState(5);
+  const [_timer, setTimer] = useState(25);
 
   const [breakTime, setBreakTime] = useState(_break * 60 );
   const [session, setSession] = useState(_timer * 60 );
@@ -29,6 +29,40 @@ const App = () => {
   let interval = null;
 
   
+
+
+  /*====================================
+    DEFINE HANDLERS
+  =====================================*/
+  const sessionINC = () => {
+    if ( session < 60 ) {
+      setSession( session + 1)
+      setTimer( _timer + 60 )
+    }
+  }
+
+  const sessinDEC = () => {
+    if ( session > 1 ) {
+      setSession( session - 1 )
+      setTimer( _timer - 60 )
+    }
+  }
+
+  const breakINC = () => {
+    if ( breakTime < 60 ) {
+      setBreakTime( breakTime + 1 )
+      setBreak( _break + 60 );
+    }
+  }
+
+  const breakDEC = () => {
+    if ( breakTime > 1 ) {
+      setBreakTime( breakTime - 1 )
+      setBreak( _break - 60 );
+    }
+  }
+
+
   return (
     <div>
       <h1>Pomodoro Clock</h1>
