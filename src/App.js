d3e140e9a4de26719f801df5f03c05ab4233506c
Darbaz Ali <darbaz.a.h@gmail.com>
2020-08-02 10:33:30 +0300

    define Hooks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { render } from "react-dom";
 
 import BreakController from "./components/BreakController";
@@ -6,6 +6,28 @@ import SessionController from "./components/SessionController";
 import Timer from './components/Timer';
 
 const App = () => {
+
+  /*====================================
+    DEFINE HOOKS
+  =====================================*/
+  const [_break, set_break] = useState(5);
+  const [_timer, set_timer] = useState(25);
+
+  const [breakTime, setBreakTime] = useState(_break * 60 );
+  const [session, setSession] = useState(_timer * 60 );
+
+  const [isRunning, setIsRunning] = useState(false);
+  const [status, setStatus] = useState('session');
+  // end of hooks
+
+
+  /*====================================
+    DEFINE VARIABLES
+  =====================================*/
+  const audio = useRef();
+  let interval = null;
+
+  
   return (
     <div>
       <h1>Pomodoro Clock</h1>
