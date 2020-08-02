672c78c6708a27074dfef9d87e29af16275e0719
Darbaz Ali <darbaz.a.h@gmail.com>
2020-08-02 09:38:02 +0300

    add classNames to BreakController

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { render } from "react-dom";
 
 import BreakController from "./components/BreakController";
-import SessionController from './components/SessionController';
+import SessionController from "./components/SessionController";
 
 const App = () => {
   return (
--- src/components/BreakController.js
+++ src/components/BreakController.js
@@ -3,13 +3,19 @@ import React from "react";
 const BreakController = (props) => {
   return (
     <div id="break-label">
-      <button id="break-decrement" onClick={props.handleBreakDec}>
-        -
-      </button>
-      <span id="break-length">{props._break || 5}</span>
-      <button id="break-increment" onClick={props.handleBreakInc}>
-        +
-      </button>
+      <div className="control-name">
+        <p>Break Length</p>
+      </div>
+
+      <div className="controls">
+        <button id="break-decrement" onClick={props.handleBreakDec}>
+          -
+        </button>
+        <span id="break-length">{props._break || 5}</span>
+        <button id="break-increment" onClick={props.handleBreakInc}>
+          +
+        </button>
+      </div>
     </div>
   );
 };
