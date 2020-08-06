241e74ce3831ba408cd2bbb60f44437d88209dec
Darbaz Ali <darbaz.a.h@gmail.com>
2020-08-06 11:26:29 +0300

    style controllers

--- src/components/BreakController.js
+++ src/components/BreakController.js
@@ -4,15 +4,15 @@ const BreakController = ({ breakDEC, breakINC, _break }) => {
   return (
     <div id="break-label">
       <div className="control-name">
-        <p>Break Length</p>
+        <p>Break(m)</p>
       </div>
 
       <div className="controls">
-        <button id="break-decrement" onClick={breakDEC}>
+        <button id="break-decrement" onClick={breakDEC} className="btn">
           -
         </button>
         <span id="break-length">{_break}</span>
-        <button id="break-increment" onClick={breakINC}>
+        <button id="break-increment" onClick={breakINC} className="btn">
           +
         </button>
       </div>
--- src/components/SessionController.js
+++ src/components/SessionController.js
@@ -4,15 +4,15 @@ const SessionController = ({ session, sessionINC, sessionDEC }) => {
   return (
     <div id="session-label">
       <div className="control-name">
-        <p>Session Length</p>
+        <p>Session(m)</p>
       </div>
 
       <div className="controls">
-        <button id="session-decrement" onClick={sessionDEC}>
+        <button id="session-decrement" onClick={sessionDEC} className="btn">
           -
         </button>
         <span id="session-length">{session}</span>
-        <button id="session-increment" onClick={sessionINC}>
+        <button id="session-increment" onClick={sessionINC} className="btn">
           +
         </button>
       </div>
